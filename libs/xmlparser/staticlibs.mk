0cb52174cf6362d060d31c0a6aa3a1a3f773bd09
Kamanashis Roy <kamanashisroy+aroopcode@gmail.com>
2014-02-19 11:19:59 +0600

    We have small makefile changes

--- libs/xmlparser/staticlibs.mk
+++ libs/xmlparser/staticlibs.mk
@@ -1,3 +1,3 @@
-XMLPARSER_CSOURCES=$(wildcard $(PROJECT_HOME)/libs/xmlparser/vsrc/*.c)
+XMLPARSER_CSOURCES=$(wildcard $(ONUBODH_HOME)/libs/xmlparser/vsrc/*.c)
 XMLPARSER_VSOURCE_BASE=$(basename $(notdir $(XMLPARSER_CSOURCES)))
-OBJECTS+=$(addprefix $(PROJECT_OBJDIR)/, $(addsuffix .o,$(XMLPARSER_VSOURCE_BASE)))
+OBJECTS+=$(addprefix $(ONUBODH_HOME)/build/.objects/, $(addsuffix .o,$(XMLPARSER_VSOURCE_BASE)))
--- transform/strtrans/staticlibs.mk
+++ transform/strtrans/staticlibs.mk
@@ -1,3 +1,3 @@
-STRTRANS_CSOURCES=$(wildcard $(PROJECT_HOME)/transform/strtrans/vsrc/*.c)
+STRTRANS_CSOURCES=$(wildcard $(ONUBODH_HOME)/transform/strtrans/vsrc/*.c)
 STRTRANS_VSOURCE_BASE=$(basename $(notdir $(STRTRANS_CSOURCES)))
-OBJECTS+=$(addprefix $(PROJECT_OBJDIR)/, $(addsuffix .o,$(STRTRANS_VSOURCE_BASE)))
+OBJECTS+=$(addprefix $(ONUBODH_HOME)/build/.objects/, $(addsuffix .o,$(STRTRANS_VSOURCE_BASE)))
